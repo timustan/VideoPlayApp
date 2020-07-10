b84c53ac9f99e79d699b76831cc5b39724e832bf
teramoto <teramoto-natsumi@nsd.co.jp>
2020-07-10 22:47:48 +0900

    URL入力キーボード追加

--- VideoPlayApp/Controller/SelectVideoViewController.swift
+++ VideoPlayApp/Controller/SelectVideoViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class SelectVideoViewController: UIViewController {
+class SelectVideoViewController: UIViewController, UITextFieldDelegate {
     
-    // 「動画を選択する」ボタン
-    @IBOutlet var selectVideoButton: CustomButton!
-    // 「URLを入力する」ボタン
-    @IBOutlet var inputURLButton: CustomButton!
-
+    // 入力バー
+    @IBOutlet var inputBarView: UIView!
+    // URL入力TextField
+    @IBOutlet var inputURLTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputURLTextField.delegate = self
+    }
+    
+    // 「動画を選択する」ボタン押下時
+    @IBAction func clickedSelectVideoButton(_ sender: Any) {
+    }
+    
+    // 「URLを入力する」ボタン押下時
+    @IBAction func clickedInputURLButton(_ sender: Any) {
+        inputURLTextField.becomeFirstResponder()
+    }
+    
+    // 「決定」ボタン押下時
+    @IBAction func clickedDecisionButton(_ sender: Any) {
+    }
+    
+    // textField以外をタッチしたらキーボードを閉じる
+    override func touchesBegan(_: Set<UITouch>, with _: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // キーボードの完了ボタン押下でキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        inputURLTextField.resignFirstResponder()
+        return true
     }
 }
