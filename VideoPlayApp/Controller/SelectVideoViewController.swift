05ab35e483bfaa93eb8c39d64b55410a5169ad66
teramoto <teramoto-natsumi@nsd.co.jp>
2020-07-10 21:46:38 +0900

    SelectVideoController作成

---
+++ VideoPlayApp/Controller/SelectVideoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SelectVideoViewController.swift
+//  VideoPlayApp
+//
+//  Created by teramoto on 2020/07/10.
+//  Copyright © 2020 natsumi. All rights reserved.
+//
+
+import UIKit
+
+class SelectVideoViewController: UIViewController {
+    
+    // 「動画を選択する」ボタン
+    @IBOutlet var selectVideoButton: CustomButton!
+    // 「URLを入力する」ボタン
+    @IBOutlet var inputURLButton: CustomButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
