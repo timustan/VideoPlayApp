e86302a46323e96347e2fd8a458eae2bc396084a
teramoto <teramoto-natsumi@nsd.co.jp>
2020-07-11 18:20:15 +0900

    ライブラリへのアクセス要求処理,UIImagePickerControllerの追加

--- VideoPlayApp/Controller/SelectVideoViewController.swift
+++ VideoPlayApp/Controller/SelectVideoViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Photos
+import CoreServices
+import os.log
 
-class SelectVideoViewController: UIViewController, UITextFieldDelegate {
+class SelectVideoViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
     // 入力バー
     @IBOutlet var inputBarView: UIView!
@@ -16,16 +19,29 @@ class SelectVideoViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var inputURLTextField: UITextField!
     // 動画再生画面遷移用identifier
     let identifier = "PlayVideoViewController"
+    // UIImagePickerController
+    let imagePickerController = UIImagePickerController()
+    // log
+    let log = OSLog(subsystem: "com.timustan.VideoPlayApp", category: "SelectVideoView")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 権限を求める
+        askForAuthority()
         inputURLTextField.delegate = self
     }
     
     // 「動画を選択する」ボタン押下時
     @IBAction func clickedSelectVideoButton(_ sender: Any) {
+        // 権限の確認　拒否状態の場合return
+        if !checkAuthority() {
+            return
+        } else {
+            // UIImagePickerを表示
+            showImagePicker()
+        }
+        
     }
-    
     // 「URLを入力する」ボタン押下時
     @IBAction func clickedInputURLButton(_ sender: Any) {
         inputURLTextField.becomeFirstResponder()
@@ -36,6 +52,49 @@ class SelectVideoViewController: UIViewController, UITextFieldDelegate {
     @IBAction func clickedDecisionButton(_ sender: Any) {
         // 動画再生画面に遷移
         self.performSegue(withIdentifier: identifier, sender: nil)
+    }
+    
+    // 権限を求める
+    func askForAuthority() {
+        // ライブラリへのアクセス許可がない場合
+        if PHPhotoLibrary.authorizationStatus() == .notDetermined {
+            // 権限を要求する
+            PHPhotoLibrary.requestAuthorization { status in
+                if status != .authorized {
+                    os_log(.default, log: self.log, "ライブラリへのアクセス拒否")
+                    
+                    return
+                }
+                os_log(.default, log: self.log, "ライブラリへのアクセス許可")
+            }
+        }
+    }
+    
+    // 権限の確認
+    func checkAuthority() -> Bool {
+        // アクセス拒否状態の場合
+        if PHPhotoLibrary.authorizationStatus() != .authorized {
+            self.showAlert(title: Const.AUTHORITY_ALERT_TITLE, message: Const.AUTHORITY_ALERT_MESSAGE)
+            return false
+        }
+        return true
+    }
+    
+    // UIImagePickerControllerの表示
+    func showImagePicker() {
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = [kUTTypeMovie as String]
+        present(imagePickerController, animated: true)
+    }
+    
+    // アラートを表示する
+    func showAlert(title: String, message: String) {
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAct: UIAlertAction = UIAlertAction(title: "OK", style: .cancel, handler: { (_: UIAlertAction!) -> Void in
+        })
+        alert.addAction(cancelAct)
+        present(alert, animated: true)
     }
     
     // textField以外をタッチしたらキーボードを閉じる
---
+++ VideoPlayApp/Utils/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  VideoPlayApp
+//
+//  Created by teramoto on 2020/07/11.
+//  Copyright © 2020 natsumi. All rights reserved.
+//
+
+import Foundation
+
+class Const {
+    // SelectVideoView
+    static let AUTHORITY_ALERT_TITLE = "動画を選択できません"
+    static let AUTHORITY_ALERT_MESSAGE = "端末の設定よりライブラリへのアクセスを許可してください。"
+}
