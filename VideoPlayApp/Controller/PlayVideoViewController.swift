823cc7791e53e3f40bb466c3a0e8ed0e38c16a62
teramoto <teramoto-natsumi@nsd.co.jp>
2020-09-03 11:27:04 +0900

    シークバーを動画の再生に応じて動くように設定

--- VideoPlayApp/Controller/PlayVideoViewController.swift
+++ VideoPlayApp/Controller/PlayVideoViewController.swift
@@ -24,6 +24,8 @@ class PlayVideoViewController: UIViewController {
     
     var player = AVPlayer()
     var URLstr = ""
+    var itemDuration: Double = 0
+    var timeObserverToken: Any?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,8 +71,56 @@ class PlayVideoViewController: UIViewController {
     
     // playerのセットアップ
     func setupPlayer () {
+        // 動画ファイルの長さを示す秒数を設定する
+        let asset = AVAsset(url: URL(string: self.URLstr)!)
+        itemDuration = CMTimeGetSeconds(asset.duration)
+        
         let playItem = AVPlayerItem(url: URL(string: self.URLstr)!)
         player = AVPlayer(playerItem: playItem)
         playerView.player = player
+        addPeriodicTimeObserver()
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let seconds = Double(sender.value) * itemDuration
+        let timeScale = CMTimeScale(NSEC_PER_SEC)
+        let time = CMTime(seconds: seconds, preferredTimescale: timeScale)
+        
+        changePosition(time: time)
+    }
+    
+    // 再生に合わせてシークバーを動かす
+    func addPeriodicTimeObserver() {
+        // Notify every half second
+        let timeScale = CMTimeScale(NSEC_PER_SEC)
+        let time = CMTime(seconds: 0.5, preferredTimescale: timeScale)
+        
+        timeObserverToken = player.addPeriodicTimeObserver(forInterval: time,
+                                                           queue: .main)
+        { [weak self] time in
+            // update player transport UI
+            DispatchQueue.main.async {
+                // sliderを更新
+                self?.updateSlider()
+            }
+        }
+    }
+    
+    private func updateSlider() {
+        let time = player.currentItem?.currentTime() ?? CMTime.zero
+        if itemDuration != 0 {
+            videoTimeSlider.value = Float(CMTimeGetSeconds(time) / itemDuration)
+        }
+    }
+    
+    private func changePosition(time: CMTime) {
+        let rate = player.rate
+        // いったんplayerをとめる
+        player.rate = 0
+        // 指定した時間へ移動
+        player.seek(to: time, completionHandler: {_ in
+            // playerをもとのrateに戻す(0より大きいならrateの速度で再生される)
+            self.player.rate = rate
+        })
     }
 }
